b6581b1c470568d2b03661c4a53a041b82015348
Jake Bolewski <jakebolewski@gmail.com>
2018-07-08 23:00:59 -0600

    wip docs

---
+++ R/Version.R
@@ -0,0 +1,9 @@
+#' The version of the libtiledb library
+#'
+#' @return An named int vector c(major, minor, patch) 
+#' @examples
+#' tiledb_version()
+#' @export
+tiledb_version <- function() {
+  libtiledb_version() 
+}
