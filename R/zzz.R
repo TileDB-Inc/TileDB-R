4227e6b45cc93ee2e6f7cd774448f783d7181fca
Peter M. Haverty <phaverty@gene.com>
2019-03-15 14:25:39 -0700

    copy paste error in comment

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,5 @@
       "ctx", tiledb_ctx(),
       eval.env = ns, assign.env = ns
   )
-  #namespaceExport(ns, "IGIS") # uncomment to export and access via tiledb::ctx rather than tiledb:::ctx
+  #namespaceExport(ns, "ctx") # uncomment to export and access via tiledb::ctx rather than tiledb:::ctx
 }
