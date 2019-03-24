fc6c090adec7808eb2717d3deca4323ef4dac8fd
Peter M. Haverty <phaverty@gene.com>
2019-03-24 07:31:31 -0700

    add utility to look up tiledb type for R type

--- R/utils.R
+++ R/utils.R
@@ -12,11 +12,11 @@ nd_index_from_syscall <- function(call, env_frame) {
                     subscript <- eval(subscript, envir = env_frame, enclos = env_frame)
                     return(subscript)
                   })
-  argnames <- tail(names(call), n = -2L) 
+  argnames <- tail(names(call), n = -2L)
   if (!is.null(argnames))
     index <- index[!(argnames %in% c("drop", "exact", "value"))]
   if (length(index) == 1L && is.null(index[[1L]]))
-    index <- list() 
+    index <- list()
   return(index)
 }
 
@@ -26,4 +26,25 @@ isNestedList <- function(l) {
     if (is.list(i)) return(TRUE)
   }
   return(FALSE)
+}
+##' Look up TileDB type corresponding to the type of an R object
+##'
+##' Look up TileDB type corresponding to the type of an R object
+##' @param x an R array or list
+##' @return single character, e.g. INT32
+##' @export
+r_to_tiledb_type <- function(x) {
+    storage_mode = storage.mode(x)
+    if (storage_mode == "list")
+        storage_mode = storage.mode(x[[1]])
+    if (storage_mode == "integer") {
+        type = "INT32"
+    } else if (storage_mode == "double"){
+        type = "FLOAT64"
+    } else if (storage_mode == "character"){
+        type = "UTF8"
+    } else {
+        message("Data type ", storage_mode, " not supported for now.")
+    }
+    type
 }
