72752c8ab64cb0c9cb809961ad57ee077e77bbf3
Dirk Eddelbuettel <edd@debian.org>
2020-01-16 09:24:44 -0600

    simplified example

--- inst/examples/ex_S3.R
+++ inst/examples/ex_S3.R
@@ -6,8 +6,7 @@ array_name <- "s3://tiledb-public-us-west-1/test-array-4x4"
 
 cfg <- tiledb_config()
 cfg["vfs.s3.region"] <- "us-west-1"
-
 ctx <- tiledb_ctx(cfg)
-tiledb_set_context(ctx)
 
-tiledb_object_type(array_name)
+## access the s3 bucket, here simply querying the type
+tiledb_object_type(array_name, ctx=ctx)
