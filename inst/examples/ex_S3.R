ba55d85c9f7255d6695030b00744338240368bcf
Dirk Eddelbuettel <edd@debian.org>
2020-01-15 18:29:41 -0600

    simple s3 example

---
+++ inst/examples/ex_S3.R
@@ -0,0 +1,13 @@
+
+library(tiledb)
+
+## a public s3 bucket
+array_name <- "s3://tiledb-public-us-west-1/test-array-4x4"
+
+cfg <- tiledb_config()
+cfg["vfs.s3.region"] <- "us-west-1"
+
+ctx <- tiledb_ctx(cfg)
+tiledb:::setContext(ctx)                # maybe export/expose that function?
+
+tiledb_object_type(array_name)
