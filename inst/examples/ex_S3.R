588c4bf4107940630e5a69a16e60e521dc8cdeda
Dirk Eddelbuettel <edd@debian.org>
2020-01-17 17:58:18 -0600

    expand comment on s3 bucket uri in s3 example

--- inst/examples/ex_S3.R
+++ inst/examples/ex_S3.R
@@ -1,7 +1,8 @@
 
 library(tiledb)
 
-## a public s3 bucket
+## replace this URI with one for a public s3 bucket,
+## or one accessible under your aws access key
 array_name <- "s3://tiledb-public-us-west-1/test-array-4x4"
 
 cfg <- tiledb_config()
