f95a0fe1268e7dc4256b668ba6f322018e4b962e
Dirk Eddelbuettel <edd@debian.org>
2020-02-12 13:44:22 -0600

    WIP snapshot, steps towards varlength unit tests

--- tests/testthat/test_VariableLength.R
+++ tests/testthat/test_VariableLength.R
@@ -3,7 +3,7 @@ context("tiledb_variable_length_arrays")
 
 tmp <- tempfile()
 
-unlink_and_create_simple <- function(tmp) {
+unlink_and_create_three_attributes <- function(tmp) {
   if (dir.exists(tmp)) unlink(tmp, recursive = TRUE, force = TRUE)
   dir.create(tmp, recursive = TRUE)
 
@@ -11,7 +11,7 @@ unlink_and_create_simple <- function(tmp) {
   dom <- tiledb_domain(dims = c(tiledb_dim("rows", c(1L, 4L), 4L, "INT32"),
                                 tiledb_dim("cols", c(1L, 4L), 4L, "INT32")))
 
-  ## The array will be dense with a single attribute "a" so each (i,j) cell can store an integer.
+  ## The array will be dense with attribute "a1" "a2" and "a3"
   schema <- tiledb_array_schema(dom,
                                 attrs = c(tiledb_attr("a1", type = "CHAR", is_var = TRUE),
                                           tiledb_attr("a2", type = "INT32", is_var = TRUE),
@@ -21,6 +21,20 @@ unlink_and_create_simple <- function(tmp) {
   tiledb_array_create(tmp, schema)
 }
 
+unlink_and_create_single_attribute <- function(tmp, attr) {
+  if (dir.exists(tmp)) unlink(tmp, recursive = TRUE, force = TRUE)
+  dir.create(tmp, recursive = TRUE)
+
+  ## The array will be 4x4 with dimensions "rows" and "cols", with domain [1,4].
+  dom <- tiledb_domain(dims = c(tiledb_dim("rows", c(1L, 4L), 4L, "INT32"),
+                                tiledb_dim("cols", c(1L, 4L), 4L, "INT32")))
+
+  ## The array will be dense with a single attribute "a" so each (i,j) cell can store an integer.
+  schema <- tiledb_array_schema(dom, attrs = c(tiledb_attr("a", type = attr, is_var = TRUE)))
+
+  ## Create the (empty) array on disk.
+  tiledb_array_create(tmp, schema)
+}
 
 ## infrastructure
 setup({
@@ -35,7 +49,7 @@ teardown({
 
 test_that("Can write and read variable length array via helpers", {
   skip_if_not_installed("data.table")
-  arr <- unlink_and_create_simple(tmp)
+  arr <- unlink_and_create_three_attributes(tmp)
 
   a1 <- data.table::data.table(V1=list("a", "eee", "i", "m"),
                                V2=list("bb", "f", "jjj", "n"),
@@ -74,9 +88,9 @@ test_that("Can write and read variable length array via helpers", {
 
 })
 
-test_that("Can write and read variable length array via [ and [<-", {
+test_that("Can read variable length array via [", {
   skip_if_not_installed("data.table")
-  unlink_and_create_simple(tmp)
+  unlink_and_create_three_attributes(tmp)
   arr <- tiledb_dense(tmp)
   a1 <- data.table::data.table(V1=list("a", "eee", "i", "m"),
                                V2=list("bb", "f", "jjj", "n"),
@@ -102,10 +116,13 @@ test_that("Can write and read variable length array via [ and [<-", {
 
   ## next three pass when test file is run individually (ie via
   ##   Rscript -e 'testthat::test_file("tests/testthat/test_VariableLength.R")'
-  ## but fail when in a package. Huh?
+  ## but fail when in a package. Huh? Forcing to list helps
   #expect_equal(arr[,1][["a1"]], a1[,1])
   #expect_equal(arr[,1][["a2"]], a2[,1])
   #expect_equal(arr[,1][["a3"]], a3[,1])
+  #expect_equal(arr[,1][["a1"]][[1]], a1[,1][[1]])
+  #expect_equal(arr[,1][["a2"]][[1]], a2[,1][[1]])
+  #expect_equal(arr[,1][["a3"]][[1]], a3[,1][[1]])
 
   expect_equal(arr[1:3,][["a1"]], a1[1:3,])
   expect_equal(arr[1:3,][["a2"]], a2[1:3,])
@@ -119,4 +136,51 @@ test_that("Can write and read variable length array via [ and [<-", {
   expect_equal(arr[2:3,1:2][["a2"]], a2[2:3,1:2])
   expect_equal(arr[2:3,1:2][["a3"]], a3[2:3,1:2])
 
+})
+
+test_that("Can read and write variable length string array", {
+  skip_if_not_installed("data.table")
+  unlink_and_create_single_attribute(tmp, "CHAR")
+  arr <- tiledb_dense(tmp)
+  a1 <- data.table::data.table(V1=list("a", "eee", "i", "m"),
+                               V2=list("bb", "f", "jjj", "n"),
+                               V3=list("ccc", "g", "kk", "oo"),
+                               V4=list("dd", "hhh", "l", "qqqq"))
+  expect_true(write_variable_length(tmp, list(a=a1)))
+  dat <- matrix(c("a",   "eee",   "i",    "m",
+                  "bb",    "f", "jjj",    "n",
+                  "ccc",   "g",  "kk",   "oo",
+                  "dd",  "hhh",   "l", "qqqq"), 4, 4)
+  #skip_if(TRUE)
+  #arr[] <- dat     # writing works with simple matrix
+  #chk <- arr[]
+  #library(data.table)
+  #dat <- data.table::data.table(dat)
+  #print(str(dat))
+  #print(str(chk))
+  #expect_equivalent(chk, dat)
+  # -- in goes a simple matrix, out comes a data.table with lists (for convenience)
+  # -- need to generalize input to let data.table in
+})
+
+test_that("Can read and write variable length int array", {
+  skip_if_not_installed("data.table")
+  unlink_and_create_single_attribute(tmp, "INT32")
+  arr <- tiledb_dense(tmp)
+  dat <- data.table::data.table(V1=list(c(1L, 1L), 5L, c(9L, 9L), 13L),
+                                V2=list(c(2L,2L), c(6L,6L), 10L, c(14L,14L,14L)),
+                                V3=list(3L, c(7L,7L), 11L, 15L),
+                                V4=list(4L, c(8L,8L,8L), c(12L,12L), 16L))
+  expect_true(write_variable_length(tmp, list(a=dat)))
+})
+
+test_that("Can read and write variable length double array", {
+  skip_if_not_installed("data.table")
+  unlink_and_create_single_attribute(tmp, "FLOAT64")
+  arr <- tiledb_dense(tmp)
+  dat <- data.table::data.table(V1=list(c(1.0, 1.1), 5,        c(9, 9),  13),
+                                V2=list(c(2,2),      c(6,6),   10,       c(14.1,14.2,14.3)),
+                                V3=list(3,           c(7,7),   11,       15),
+                                V4=list(4,           c(8,8,8), c(12,12), 16.75))
+  expect_true(write_variable_length(tmp, list(a=dat)))
 })
