7d3047009ddf7a690b9f7a6ac25155e1f7d197c2
Dirk Eddelbuettel <edd@debian.org>
2020-03-04 12:27:15 -0600

    Add test for int32 variable array

--- tests/testthat/test_DenseArray.R
+++ tests/testthat/test_DenseArray.R
@@ -792,5 +792,124 @@ test_that("low-level variable-length character array write and read works", {
   expect_equal(tiledb:::libtiledb_query_status(qryptr), "INCOMPLETE")
   tiledb:::libtiledb_array_close(arrptr)
 
+})
+
+
+test_that("low-level variable-length int32 array write and read works", {
+  array_name <- tempfile()
+  setup({
+    unlink_and_create(array_name)
+  })
+
+  ## The array will be 4x4 with dimensions "rows" and "cols", with domain [1,4].
+  dom <- tiledb_domain(dims = c(tiledb_dim("rows", c(1L, 4L), 4L, "INT32"),
+                                tiledb_dim("cols", c(1L, 4L), 4L, "INT32")))
+
+
+  attr <- tiledb_attr("a", type = "INT32")
+  ## set to variable length
+  tiledb:::libtiledb_attribute_set_cell_val_num(attr@ptr, NA)
+
+  ## now set the schema
+  ctx <- tiledb_ctx()
+  schptr <- tiledb:::libtiledb_array_schema_create(ctx@ptr, "DENSE")
+  tiledb:::libtiledb_array_schema_set_domain(schptr, dom@ptr)
+  tiledb:::libtiledb_array_schema_set_cell_order(schptr, "ROW_MAJOR")
+  tiledb:::libtiledb_array_schema_set_tile_order(schptr, "ROW_MAJOR")
+  tiledb:::libtiledb_array_schema_add_attribute(schptr, attr@ptr)
+
+  ## Create the (empty) array on disk.
+  tiledb:::libtiledb_array_create(array_name, schptr)
+
+
+  data <- c(1L, 1L, 2L, 2L, 3L, 4L, 5L, 6L, 6L, 7L, 7L, 8L, 8L, 8L, 9L, 9L, 10L,
+            11L, 12L, 12L, 13L, 14L, 14L, 14L, 15L, 16L)
+  offsets <- c(0L, 2L, 4L, 5L, 6L, 7L, 9L, 11L, 14L, 16L, 17L, 18L, 20L, 21L, 24L, 25L)
+  offsets <- offsets * 4 # known fixed size of integer
+
+
+  #ctx <- tiledb_ctx()
+  arrptr <- tiledb:::libtiledb_array_open(ctx@ptr, array_name, "WRITE")
+  qryptr <- tiledb:::libtiledb_query(ctx@ptr, arrptr, "WRITE")
+  qryptr <- tiledb:::libtiledb_query_set_layout(qryptr, "ROW_MAJOR")
+
+  bufptr <- tiledb:::libtiledb_query_buffer_var_vec_create(offsets, data)
+  qryptr <- tiledb:::libtiledb_query_set_buffer_var_vec(qryptr, "a", bufptr)
+  qryptr <- tiledb:::libtiledb_query_submit(qryptr)
+  expect_equal(tiledb:::libtiledb_query_status(qryptr), "COMPLETE")
+  tiledb:::libtiledb_array_close(arrptr)
+
+  ## read and test
+  #ctx <- tiledb_ctx()
+  subarr <- NULL
+  arrptr <- tiledb:::libtiledb_array_open(ctx@ptr, array_name, "READ")
+  if (is.null(subarr)) {
+      schptr <- tiledb:::libtiledb_array_get_schema(arrptr)
+      domptr <- tiledb:::libtiledb_array_schema_get_domain(schptr)
+      lst <- tiledb:::libtiledb_domain_get_dimensions(domptr)
+      subarr <- c(tiledb:::libtiledb_dim_get_domain(lst[[1]]),
+                  tiledb:::libtiledb_dim_get_domain(lst[[2]]))
+  }
+  bufptr <- tiledb:::libtiledb_query_buffer_var_vec_alloc(arrptr, subarr, "a")
+
+  qryptr <- tiledb:::libtiledb_query(ctx@ptr, arrptr, "READ")
+  qryptr <- tiledb:::libtiledb_query_set_subarray(qryptr, subarr)
+  qryptr <- tiledb:::libtiledb_query_set_layout(qryptr, "ROW_MAJOR")
+
+  qryptr <- tiledb:::libtiledb_query_set_buffer_var_vec(qryptr, "a", bufptr)
+  qryptr <- tiledb:::libtiledb_query_submit(qryptr)
+  expect_equal(tiledb:::libtiledb_query_status(qryptr), "COMPLETE")
+  tiledb:::libtiledb_array_close(arrptr)
+
+  rl <- tiledb:::libtiledb_query_get_buffer_var_vec(qryptr, "a", bufptr)
+  expect_equal(rl[[1]], offsets)
+  expect_equal(rl[[2]], data)
+
+
+
+  data <- c(11L, 11L, 22L, 22L, 33L, 44L)
+  offsets <- c(0L, 2L, 4L, 5L)
+  offsets <- offsets * 4 # known fixed size of integer
+
+  subarr <- c(2L,3L, 2L,3L)
+
+  #ctx <- tiledb_ctx()
+  arrptr <- tiledb:::libtiledb_array_open(ctx@ptr, array_name, "WRITE")
+  qryptr <- tiledb:::libtiledb_query(ctx@ptr, arrptr, "WRITE")
+  qryptr <- tiledb:::libtiledb_query_set_subarray(qryptr, subarr)
+  qryptr <- tiledb:::libtiledb_query_set_layout(qryptr, "ROW_MAJOR")
+
+  bufptr <- tiledb:::libtiledb_query_buffer_var_vec_create(offsets, data)
+  qryptr <- tiledb:::libtiledb_query_set_buffer_var_vec(qryptr, "a", bufptr)
+  qryptr <- tiledb:::libtiledb_query_submit(qryptr)
+  expect_equal(tiledb:::libtiledb_query_status(qryptr), "COMPLETE")
+  tiledb:::libtiledb_array_close(arrptr)
+
+
+  ## read and test again
+  #ctx <- tiledb_ctx()
+  subarr <- c(2L,3L, 2L,3L)
+  arrptr <- tiledb:::libtiledb_array_open(ctx@ptr, array_name, "READ")
+  if (is.null(subarr)) {
+      schptr <- tiledb:::libtiledb_array_get_schema(arrptr)
+      domptr <- tiledb:::libtiledb_array_schema_get_domain(schptr)
+      lst <- tiledb:::libtiledb_domain_get_dimensions(domptr)
+      subarr <- c(tiledb:::libtiledb_dim_get_domain(lst[[1]]),
+                  tiledb:::libtiledb_dim_get_domain(lst[[2]]))
+  }
+  bufptr <- tiledb:::libtiledb_query_buffer_var_vec_alloc(arrptr, subarr, "a")
+
+  qryptr <- tiledb:::libtiledb_query(ctx@ptr, arrptr, "READ")
+  qryptr <- tiledb:::libtiledb_query_set_subarray(qryptr, subarr)
+  qryptr <- tiledb:::libtiledb_query_set_layout(qryptr, "ROW_MAJOR")
+
+  qryptr <- tiledb:::libtiledb_query_set_buffer_var_vec(qryptr, "a", bufptr)
+  qryptr <- tiledb:::libtiledb_query_submit(qryptr)
+  expect_equal(tiledb:::libtiledb_query_status(qryptr), "COMPLETE")
+  tiledb:::libtiledb_array_close(arrptr)
+
+  rl <- tiledb:::libtiledb_query_get_buffer_var_vec(qryptr, "a", bufptr)
+  expect_equal(rl[[1]], offsets)
+  expect_equal(rl[[2]], data)
 
 })
