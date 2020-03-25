5cf7525b70982dbfa6d78490cb5c014e28fedd1f
Dirk Eddelbuettel <edd@debian.org>
2020-03-25 10:38:34 -0500

    Add unit test for fromDataFrame

---
+++ tests/testthat/test_DataFrame.R
@@ -0,0 +1,19 @@
+library(tiledb)
+context("tiledb_dataframe")
+
+test_that("tiledb_fromdataframe", {
+  uri <- tempfile()
+  ## turn factor into character
+  irisdf <- within(iris, Species <- as.character(Species))
+
+  fromDataFrame(irisdf, uri)
+
+  arr <- tiledb_dense(uri, as.data.frame=TRUE)
+  newdf <- arr[]
+  expect_equal(iris, newdf)
+  expect_equal(dim(irisdf), dim(newdf))
+
+  newdf <- within(newdf, Species <- as.character(Species))
+  expect_equal(irisdf, newdf)
+
+ })
