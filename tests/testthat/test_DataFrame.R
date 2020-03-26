94f53434ee528eba393b70dae215132ece4561ef
Dirk Eddelbuettel <edd@debian.org>
2020-03-26 14:18:58 -0500

    Additional unit test

--- tests/testthat/test_DataFrame.R
+++ tests/testthat/test_DataFrame.R
@@ -13,8 +13,27 @@ test_that("tiledb_fromdataframe", {
   expect_equal(iris, newdf)
   expect_equal(dim(irisdf), dim(newdf))
 
+  ## result comes back as factor by default
   newdf <- within(newdf, Species <- as.character(Species))
   expect_equal(irisdf, newdf)
-
 })
 
+test_that("tiledb_date_datetime_types", {
+  uri <- tempfile()
+
+  df <- data.frame(char=c("abc", "def", "g", "hijk"),
+                   int=1:4,
+                   dbl=sqrt(1:4),
+                   date=Sys.Date() + 0:3,
+                   stringsAsFactors = FALSE)
+
+  fromDataFrame(df, uri)
+
+  arr <- tiledb_dense(uri, as.data.frame=TRUE)
+  newdf <- arr[]
+
+  ## result comes back as factor by default
+  newdf <- within(newdf, char <- as.character(char))
+
+  expect_equal(df, newdf)
+})
