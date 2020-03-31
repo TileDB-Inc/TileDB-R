e7c0514ecf863331c4b43c6539ea1f684fcdd272
Dirk Eddelbuettel <edd@debian.org>
2020-03-30 19:35:45 -0500

    Add ex_3 for two-dim sparse heterogeneous array (wip)

--- inst/examples/ex_3.R
+++ inst/examples/ex_3.R
@@ -1,22 +1,22 @@
-
 library(tiledb)
 
 ## Name of the array to create.
-array_name <- "ex_2"
+array_name <- "ex_3"
 ## Path is either current directory, or a local config value is found
 uri <- file.path(getOption("TileDB_Data_Path", "."), array_name)
 
 create_array <- function(uri) {
-    # Check if the array already exists.
-    if (tiledb_object_type(uri) == "ARRAY") {
-        message("Array already exists.")
-        return(invisible(NULL))
-    }
+  ## Check if the array already exists.
+  if (tiledb_object_type(uri) == "ARRAY") {
+    message("Array already exists.")
+    return(invisible(NULL))
+  }
 
   ## The array will be 10x5 with dimensions "rows" and "cols", with domains [1,10] and [1,5].
   dom <- tiledb_domain(dims = c(tiledb_dim("rows", c(ISOdatetime(2010,1,1,0,0,0),
                                                      ISOdatetime(2029,12,31,23,59,59.999)),
-                                           10L, type = "DATETIME_MS")))
+                                           10L, type = "DATETIME_MS"),
+                                tiledb_dim("cols", domain=c(1L,5L), tile=1L, type = "INT32")))
 
   ## The array will be dense with a single attribute "a" so each (i,j) cell can store an integer.
   schema <- tiledb_array_schema(dom, attrs = c(tiledb_attr("a", type = "INT32"),
@@ -31,35 +31,15 @@ create_array <- function(uri) {
 }
 
 write_array <- function(uri) {
-  #data <- list(array(seq(1:10), dim = c(10,1)),
-  #             array(as.double(seq(101,110)), dim = c(10,1)),
-  #             #array(c(letters[1:26], "brown", "fox", LETTERS[1:22])[1:10], dim = c(10,1)),
-  #             array(ISOdate(2020,1,1) + cumsum(runif(10)*5), dim=c(10,1)),
-  #             array(ISOdatetime(2020,1,1,0,0,0) + cumsum(rnorm(10) * 1e5), dim=c(10,1)))
-  data <- list(seq(1:10),
-               as.double(seq(101,110)),
+  data <- list(seq(1:50),
+               as.double(seq(101,150)),
                #c(letters[1:26], "brown", "fox", LETTERS[1:22])[1:10],
-               as.Date("2020-01-01") + cumsum(runif(10)*5),
-               ISOdatetime(2020,1,1,6,0,0) + cumsum(rnorm(10) * 1e5))
+               as.Date("2020-01-01") + cumsum(runif(50)*5),
+               ISOdatetime(2020,1,1,6,0,0) + cumsum(rnorm(50) * 1e5))
   ## Open the array and write to it.
   A <- tiledb_sparse(uri = uri)
-  coords <- ISOdatetime(2020,1,1,0,0,0) + (1:10)*60
+  coords <- ISOdatetime(2020,1,1,0,0,0) + (1:50)*60
   A[coords] <- data
-}
-
-read_array <- function(uri) {
-  ## Open the array and read from it.
-  A <- tiledb_sparse(uri = uri)
-  data <- A[]
-  show(data)
-  ##schema(A)
-
-}
-
-read_as_df <- function(uri) {
-  A <- tiledb_sparse(uri = uri, as.data.frame = TRUE)
-  data <- A[1577858580:1577858700]
-  show(data)
 }
 
 set.seed(42)
@@ -68,6 +48,3 @@ if (!dir.exists(array_name)) {
   create_array(uri)
   write_array(uri)
 }
-read_array(uri)
-read_as_df(uri)
-cat("Done.\n")
