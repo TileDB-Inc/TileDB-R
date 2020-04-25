b8a28b0e92c7ad70087772206d56a045801c930c
Dirk Eddelbuettel <edd@debian.org>
2020-04-24 20:45:24 -0500

    Add makeInteger64 helper

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -23,3 +23,18 @@ Rcpp::NumericVector makeNanotime(const std::vector<int64_t>& vec) {
 
   return(num);
 }
+
+// Create a integer64 object
+//
+// Integer64 is an S3 class. For more on this see e.g.
+// https://gallery.rcpp.org/articles/creating-integer64-and-nanotime-vectors/
+//
+Rcpp::NumericVector makeInteger64(const std::vector<int64_t>& vec) {
+  size_t n = vec.size();
+
+  Rcpp::NumericVector num(n);
+  std::memcpy(&(num[0]), vec.data(), n*sizeof(double));
+
+  num.attr("class") = "integer64";
+  return(num);
+}
