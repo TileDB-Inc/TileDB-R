e6473544f113be8b94be9facf6b1a15664a4546a
Dirk Eddelbuettel <edd@debian.org>
2020-05-13 14:36:21 -0500

    Modified configure and helper script to use shared library

--- src/tools/fetchTileDB.sh
+++ src/tools/fetchTileDB.sh
@@ -2,15 +2,20 @@
 
 tarball=tiledb-ubuntu-16.04-shared.tar.gz
 
-test -f /etc/os-release && echo "** On " && cat /etc/os-release
+#test -f /etc/os-release && echo "** On " && cat /etc/os-release
 
 if [ ! -f tiledb_build.tar.gz ]; then
     curl -s -k -L -O http://dirk.eddelbuettel.com/tmp/${tarball}
 fi
 
-if [ ! -f tiledb ]; then
+if [ -d tiledb ]; then
+    rm -rfv tiledb
+fi
+
+if [ ! -d tiledb ]; then
     mkdir tiledb
     tar xaf ${tarball} -C tiledb
-    cd tiledb/lib && mv -v libtiledb.so.?.? libtiledb.so
-    ls -lR tiledb/
+    ## making a full copy is inelegant but apparently needed
+    cd tiledb/lib && cp -vax libtiledb.so.?.? libtiledb.so && cd -
+    #ls -lR tiledb/
 fi
