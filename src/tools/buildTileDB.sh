a82ff1e543ed3c785703550d033a7e8cb997cfa9
Dirk Eddelbuettel <edd@debian.org>
2020-06-12 12:00:14 -0500

    Prefix base url to archive file

--- src/tools/buildTileDB.sh
+++ src/tools/buildTileDB.sh
@@ -10,7 +10,7 @@ cd src
 if [ ! -f tiledb.tar.gz ]; then
     url=$(curl -Ls https://github.com/TileDB-Inc/TileDB/releases/latest | sed -n -e 's/.*href=\"\(.*tar.gz\)".*/\1/p' | grep archive)
     echo "Downloading ${url} as tiledb.tar.gz ..."
-    curl -s -k -L -o tiledb.tar.gz ${url}
+    curl -s -k -L -o tiledb.tar.gz "https://github.com/TileDB-Inc/TileDB/archive/"${url}
 fi
 
 if [ ! -d tiledb-src ]; then
