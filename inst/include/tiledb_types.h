78adc33b0e59cda62b39ac1a35b4a5d56329be2e
Dirk Eddelbuettel <edd@debian.org>
2020-07-01 09:53:44 -0500

    add tiledb_types.h

---
+++ inst/include/tiledb_types.h
@@ -0,0 +1,7 @@
+
+// Rcpp Attributes create RcppExports.cpp -- and one of the implemented conventions is
+// to, as an option, #include a header file named as package follow by _types.h if present
+
+#ifndef TILEDB_DEPRECATED
+  #define TILEDB_DEPRECATED
+#endif
