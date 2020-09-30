ea595734351d0f2042d81ef451c7cb57bc9c5ea6
Dirk Eddelbuettel <edd@debian.org>
2020-09-30 08:12:27 -0500

    update for 2.1.0 rc1

--- tools/fetchTileDBLib.R
+++ tools/fetchTileDBLib.R
@@ -19,8 +19,8 @@ urlarg <- argv[2]
 
 baseurl <- "https://github.com/TileDB-Inc/TileDB/releases/download"
 dlurl <- switch(osarg,
-                linux = file.path(baseurl,"2.0.8/tiledb-linux-2.0.8-db41376-without_tbb.tar.gz"),
-                macos = file.path(baseurl,"2.0.8/tiledb-macos-2.0.8-db41376-without_tbb.tar.gz"),
+                linux = file.path(baseurl,"2.1.0-rc1/tiledb-linux-2.1.0-rc1-4cffc77-without_tbb.tar.gz"),
+                macos = file.path(baseurl,"2.1.0-rc1/tiledb-macos-2.1.0-rc1-4cffc77-without_tbb.tar.gz"),
                 url = urlarg)
 cat("downloading", dlurl, "\n")
 download.file(dlurl, "tiledb.tar.gz", quiet=TRUE)
