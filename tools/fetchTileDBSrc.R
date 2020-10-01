86128580083fcda5516fd107190f5633f399c8dd
Dirk Eddelbuettel <edd@debian.org>
2020-10-01 14:28:46 -0500

    update for 2.1 rc3

--- tools/fetchTileDBSrc.R
+++ tools/fetchTileDBSrc.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 ## by default we download the source from a given release
-url <- "https://github.com/TileDB-Inc/TileDB/archive/2.1.0-rc2.tar.gz"
+url <- "https://github.com/TileDB-Inc/TileDB/archive/2.1.0-rc3.tar.gz"
 
 cat("Downloading ", url, "\n")
 download.file(url, "tiledb.tar.gz", quiet=TRUE)
