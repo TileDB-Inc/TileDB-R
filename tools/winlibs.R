2233df102d9eee8f6578faadcfa1a74ce4e59248
Dirk Eddelbuettel <edd@debian.org>
2020-10-15 09:57:11 -0500

    (re)set timeout for download to accomodate CRAN using lower default

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,10 @@
 VERSION <- commandArgs(TRUE)
 if (!file.exists(sprintf("../windows/tiledb-%s/include/tiledb/tiledb.h", VERSION))) {
   if (getRversion() < "4") stop("This package requires Rtools40 or newer")
+  op <- options()
+  options(timeout=60)
   download.file(sprintf("https://github.com/rwinlib/tiledb/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+  options(op)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
