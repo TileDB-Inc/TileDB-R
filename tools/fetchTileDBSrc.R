26c05e589a5ada91e261bf6a4046ecd34a94d4c3
Dirk Eddelbuettel <edd@debian.org>
2020-10-15 16:13:25 -0500

    use release 2.1.1

--- tools/fetchTileDBSrc.R
+++ tools/fetchTileDBSrc.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 ## by default we download the source from a given release
-url <- "https://github.com/TileDB-Inc/TileDB/archive/2.1.0.tar.gz"
+url <- "https://github.com/TileDB-Inc/TileDB/archive/2.1.1.tar.gz"
 
 cat("Downloading ", url, "\n")
 op <- options()
