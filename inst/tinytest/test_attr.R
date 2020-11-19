91a8bf34c26bf24a0fd840a3e5b8e8306a5e0639
Dirk Eddelbuettel <edd@debian.org>
2020-11-19 14:44:29 -0600

    added datetime tests for attributes

--- inst/tinytest/test_attr.R
+++ inst/tinytest/test_attr.R
@@ -89,3 +89,57 @@ expect_equal(tiledb:::libtiledb_attribute_get_fill_value(attr@ptr), 42)
 
 if (dir.exists(uri)) unlink(uri, recursive=TRUE)
 #})
+
+
+## datetimes test (cf ex_aggdatetimes)
+suppressMessages({
+  library(nanotime)
+  library(bit64)
+})
+dimtype <- "INT32"
+intmax <- .Machine$integer.max         # shorthand
+uri <- tempfile()
+if (dir.exists(uri)) unlink(uri, recursive=TRUE)
+
+tile <- 1000L
+domain <- tiledb_domain(tiledb_dim("row", c(-intmax,intmax), tile, dimtype))
+attrib <- c(tiledb_attr("year",  type = "DATETIME_YEAR"),  # year
+            tiledb_attr("month", type = "DATETIME_MONTH"), # month
+            tiledb_attr("week",  type = "DATETIME_WEEK"),  # week
+            tiledb_attr("day",   type = "DATETIME_DAY"),   # date
+            tiledb_attr("hr",    type = "DATETIME_HR"),    # hour
+            tiledb_attr("min",   type = "DATETIME_MIN"),   # minute
+            tiledb_attr("sec",   type = "DATETIME_SEC"),   # second
+            tiledb_attr("ms",    type = "DATETIME_MS"),    # millisecond
+            tiledb_attr("us",    type = "DATETIME_US"),    # microsecond
+            tiledb_attr("ns",    type = "DATETIME_NS"),    # nanosecond
+            tiledb_attr("ps",    type = "DATETIME_PS"),    # picosecond
+            tiledb_attr("fs",    type = "DATETIME_FS"),    # femtosecond
+            tiledb_attr("as",    type = "DATETIME_AS")     # attosecond
+            )
+schema <- tiledb_array_schema(domain, attrib, sparse=TRUE)
+res <- tiledb_array_create(uri, schema)
+
+arr <- tiledb_array(uri, as.data.frame=TRUE)
+
+dvec <- 1:3
+data <- data.frame(row = dvec,
+                   year  = c(as.Date("2020-01-01"), as.Date("2021-01-01"), as.Date("2022-01-01")),
+                   month = c(as.Date("2020-01-01"), as.Date("2020-02-01"), as.Date("2020-03-01")),
+                   week  = c(as.Date("2020-01-01"), as.Date("2020-01-08"), as.Date("2020-01-15")),
+                   day   = as.Date("2020-01-01") + 0:2,
+                   hr    = as.POSIXct("2020-01-01 00:00:00") + (0:2)*3600,
+                   min   = as.POSIXct("2020-01-01 00:00:00") + (0:2)*60,
+                   sec   = as.POSIXct("2020-01-01 00:00:00") + (0:2),
+                   ms    = as.POSIXct("2000-01-01 00:00:00") + (0:2)*3600 + rep(0.001,3),
+                   us    = as.POSIXct("2000-01-01 00:00:00") + (0:2)*3600 + rep(0.000002,3),
+                   ns    = as.nanotime("1970-01-01T00:00:00.000000001+00:00") + (0:2)*1e9,
+                   ps    = as.nanotime("1970-01-01T00:00:00.000000001+00:00") + (0:2)*1e9,
+                   fs    = as.nanotime("1970-01-01T00:00:00.000000001+00:00") + (0:2)*1e9,
+                   as    = as.nanotime("1970-01-01T00:00:00.000000001+00:00") + (0:2)*1e9
+                   )
+
+arr[] <- data
+arr2 <- tiledb_array(uri, as.data.frame=TRUE)
+readdata <- arr2[]
+expect_true(all.equal(data, readdata))
