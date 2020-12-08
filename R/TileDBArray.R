f5df895fbf602eb0af0427292b4cc92441839870
Dirk Eddelbuettel <edd@debian.org>
2020-12-08 10:20:43 -0600

    add tests for schema-from-char, correct one comment

--- R/TileDBArray.R
+++ R/TileDBArray.R
@@ -140,7 +140,7 @@ setMethod("schema", "tiledb_array", function(object, ...) {
   return(tiledb_array_schema.from_ptr(schema_xptr))
 })
 
-## unexported helper function to deal with ... args / enckey in next two method
+## unexported helper function to deal with ... args / enckey in next method
 .array_schema_load <- function(ctxptr, uri, enckey=character()) {
   if (length(enckey) > 0) {
     schema_xptr <- libtiledb_array_schema_load_with_key(ctxptr, uri, enckey)
