b94196258cb86a10b054a308f923549bd4965957
Dirk Eddelbuettel <edd@debian.org>
2020-12-28 17:35:33 -0600

    additional time-travel support

--- inst/tinytest/test_tiledbarray.R
+++ inst/tinytest/test_tiledbarray.R
@@ -989,3 +989,37 @@ expect_equal(schema, schema2)
 schema3 <- schema(arr)
 expect_true(is(schema3, "tiledb_array_schema"))
 expect_equal(schema, schema3)
+
+
+## time travel
+tmp <- tempfile()
+dir.create(tmp)
+dom <- tiledb_domain(dims = c(tiledb_dim("rows", c(1L, 10L), 5L, "INT32"),
+                              tiledb_dim("cols", c(1L, 10L), 5L, "INT32")))
+schema <- tiledb_array_schema(dom, attrs=c(tiledb_attr("a", type = "INT32")), sparse = TRUE)
+invisible( tiledb_array_create(tmp, schema) )
+
+I <- c(1, 2, 2)
+J <- c(1, 4, 3)
+data <- c(1L, 2L, 3L)
+now1 <- Sys.time()
+A <- tiledb_array(uri = tmp, timestamp=now1)
+A[I, J] <- data
+
+Sys.sleep(1)
+
+now2 <- Sys.time()
+I <- c(8, 6, 9)
+J <- c(5, 7, 8)
+data <- c(11L, 22L, 33L)
+A <- tiledb_array(uri = tmp, timestamp=now2)
+A[I, J] <- data
+
+A <- tiledb_array(uri = tmp, as.data.frame=TRUE, timestamp=now1 - 0.5)
+expect_equal(nrow(A[]), 0)
+A <- tiledb_array(uri = tmp, as.data.frame=TRUE, timestamp=now1 + 0.5)
+expect_equal(nrow(A[]), 3)
+A <- tiledb_array(uri = tmp, as.data.frame=TRUE, timestamp=now2 - 0.5)
+expect_equal(nrow(A[]), 3)
+A <- tiledb_array(uri = tmp, as.data.frame=TRUE, timestamp=now2 + 0.5)
+expect_equal(nrow(A[]), 6)
