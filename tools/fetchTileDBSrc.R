5d47f3a500f5b923dfea7bd4703e18bdc189657a
Dirk Eddelbuettel <edd@debian.org>
2021-01-26 11:28:48 -0600

    update to TileDB 2.2.2 (#206)

--- tools/fetchTileDBSrc.R
+++ tools/fetchTileDBSrc.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 ## by default we download the source from a given release
-url <- "https://github.com/TileDB-Inc/TileDB/archive/2.2.1.tar.gz"
+url <- "https://github.com/TileDB-Inc/TileDB/archive/2.2.2.tar.gz"
 
 cat("Downloading ", url, "\n")
 op <- options()
