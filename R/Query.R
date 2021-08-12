a4be6cf64f1fde36bbf0373ef42f18e02222b40f
Dirk Eddelbuettel <edd@debian.org>
2021-08-12 09:45:16 -0500

    add vector-valued query buffer accessor and document difference

--- R/Query.R
+++ R/Query.R
@@ -298,17 +298,50 @@ tiledb_query_status <- function(query) {
   libtiledb_query_status(query@ptr)
 }
 
-#' Get TileDB Query result buffer elements
+#' Get TileDB Query result buffer element size
+#'
+#' The underlying library functions returns a pair of values as a vector
+#' of length two. The first number is the number of element offsets for variable
+#' size attributes (and always zero for fixed-sized attributes and coordinates).
+#' The second is the number of elements in the data buffer. For variable-sized
+#' attributes the first number is the number of cells read (and hence the number
+#' of offsets), the second number is the number of elements in the data buffer.
+#'
+#' As this function was first made available when only a scalar (corresponding to
+#' the second result) was returned, we still return that value.
 #'
 #' @param query A TileDB Query object
 #' @param attr A character value containing the attribute
 #' @return A integer with the number of elements in the results buffer
 #' for the given attribute
+#' @seealso tiledb_query_result_buffer_elements_vec
 #' @export
 tiledb_query_result_buffer_elements <- function(query, attr) {
   stopifnot(query_object=is(query, "tiledb_query"),
             attr_variable=is.character(attr))
   libtiledb_query_result_buffer_elements(query@ptr, attr)
+}
+
+#' Get TileDB Query result buffer element size pair as vector
+#'
+#' The underlying library functions returns a pair of values as a vector
+#' of length two. The first number is the number of element offsets for variable
+#' size attributes (and always zero for fixed-sized attributes and coordinates).
+#' The second is the number of elements in the data buffer. For variable-sized
+#' attributes the first number is the number of cells read (and hence the number
+#' of offsets), the second number is the number of elements in the data buffer.
+#'
+#' @param query A TileDB Query object
+#' @param attr A character value containing the attribute
+#' @return A vector with the number of elements in the offsets buffer (and zero
+#' for fixed-size attribute or dimensions) and the number elements in the results
+#' buffer #' for the given attribute
+#' @seealso tiledb_query_result_buffer_elements
+#' @export
+tiledb_query_result_buffer_elements_vec <- function(query, attr) {
+  stopifnot(query_object=is(query, "tiledb_query"),
+            attr_variable=is.character(attr))
+  libtiledb_query_result_buffer_elements_vec(query@ptr, attr)
 }
 
 #' Set a range for a given query
