280f9d71f0e3b4b7d1cb8f0209b683ccd748f5d1
Dirk Eddelbuettel <edd@debian.org>
2021-09-30 17:49:02 -0500

    add tests

---
+++ inst/tinytest/test_matrix.R
@@ -0,0 +1,32 @@
+
+library(tinytest)
+library(tiledb)
+
+isOldWindows <- Sys.info()[["sysname"]] == "Windows" && grepl('Windows Server 2008', osVersion)
+if (isOldWindows) exit_file("skip this file on old Windows releases")
+
+ctx <- tiledb_ctx(limitTileDBCores())
+
+uri <- tempfile()
+M <- matrix(1:12, 3, 4, dimnames=list(LETTERS[1:3], letters[1:4]))
+fromMatrix(M, uri)
+
+M2 <- toMatrix(uri)
+expect_equivalent(M, M2)
+
+
+uri <- tempfile()
+M <- matrix(1:20, 2, 10)                # matrix without rownames
+fromMatrix(M, uri)
+
+M2 <- toMatrix(uri)
+expect_equivalent(M, M2)
+
+
+
+uri <- tempfile()
+M <- matrix(sqrt(1:20), 5, 4)           # matrix without rownames, float values
+fromMatrix(M, uri)
+
+M3 <- toMatrix(uri)
+expect_equivalent(M, M3)
