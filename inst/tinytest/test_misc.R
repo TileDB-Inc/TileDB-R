f0c0d8b8e7f3840a3d5743729e3dce4d7e14fa8d
Dirk Eddelbuettel <edd@debian.org>
2021-12-02 10:24:13 -0600

    relax minimum version for test to 2.6.0 (#332)

--- inst/tinytest/test_misc.R
+++ inst/tinytest/test_misc.R
@@ -7,7 +7,7 @@ if (isOldWindows) exit_file("skip this file on old Windows releases")
 
 ctx <- tiledb_ctx(limitTileDBCores())
 
-if (tiledb_version(TRUE) < "2.5.1") exit_file("These tests require TileDB 2.5.1 or newer")
+if (tiledb_version(TRUE) < "2.6.0") exit_file("These tests require TileDB 2.6.0 or newer")
 
 if (is.null(get0("tiledb_error_message"))) exit_file("No 'tiledb_error_message'")
 expect_true(inherits(tiledb_error_message(), "character"))
