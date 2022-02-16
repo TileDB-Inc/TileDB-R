3d327713b65615be17fb67314bf3ca6752aa4d1a
Dirk Eddelbuettel <edd@debian.org>
2022-02-16 11:37:14 -0600

    update Pointer struct

--- src/arrowio.cpp
+++ src/arrowio.cpp
@@ -27,17 +27,72 @@
 #endif
 
 
-// borrowed from arrow R package (licensed under Apache-2.0) and slightly extended
+// // borrowed from arrow R package (version 2.0.0 ?) (licensed under Apache-2.0) and slightly extended
+// template <typename T>
+// struct Pointer {
+//     Pointer() : ptr_(new T()) {}
+//     explicit oldPointer(SEXP x) : ptr_(reinterpret_cast<T*>(static_cast<uintptr_t>(REAL(x)[0]))) {}
+//     inline operator SEXP() const { return Rf_ScalarReal(static_cast<double>(reinterpret_cast<uintptr_t>(ptr_)));  }
+//     inline operator T*()   const { return ptr_; }
+//     inline void finalize()       { delete ptr_; }
+//     inline T* get()        const { return ptr_; }
+//     T* ptr_;
+// };
+
+// borrowed from arrow R package (version 7.0.0)  (licensed under Apache-2.0) and slightly extended/adapted
 template <typename T>
 struct Pointer {
     Pointer() : ptr_(new T()) {}
-    explicit Pointer(SEXP x) : ptr_(reinterpret_cast<T*>(static_cast<uintptr_t>(REAL(x)[0]))) {}
-    inline operator SEXP() const { return Rf_ScalarReal(static_cast<double>(reinterpret_cast<uintptr_t>(ptr_)));  }
-    inline operator T*()   const { return ptr_; }
-    inline void finalize()       { delete ptr_; }
-    inline T* get()        const { return ptr_; }
+    explicit Pointer(SEXP x) {
+        if (TYPEOF(x) == EXTPTRSXP) {
+            ptr_ = (T*)R_ExternalPtrAddr(x);
+
+        } else if (TYPEOF(x) == STRSXP && Rf_length(x) == 1) {
+            // User passed a character representation of the pointer address
+            SEXP char0 = STRING_ELT(x, 0);
+            if (char0 == NA_STRING) {
+                Rcpp::stop("Can't convert NA_character_ to pointer");
+            }
+
+            const char* input_chars = CHAR(char0);
+            char* endptr;
+            uint64_t ptr_value = strtoull(input_chars, &endptr, 0);
+            if (endptr != (input_chars + strlen(input_chars))) {
+                Rcpp::stop("Can't parse '%s' as a 64-bit integer address", input_chars);
+            }
+            ptr_ = reinterpret_cast<T*>(static_cast<uintptr_t>(ptr_value));
+
+        } else if (Rf_inherits(x, "integer64") && Rf_length(x) == 1) {
+            // User passed an integer64(1) of the pointer address an integer64 is a REALSXP
+            // under the hood, with the bytes of each double reinterpreted as an int64.
+            uint64_t ptr_value;
+            memcpy(&ptr_value, REAL(x), sizeof(uint64_t));
+            ptr_ = reinterpret_cast<T*>(static_cast<uintptr_t>(ptr_value));
+
+        } else if (TYPEOF(x) == RAWSXP && Rf_length(x) == sizeof(T*)) {
+            // User passed a raw(<pointer size>) with the literal bytes of the pointer.
+            memcpy(&ptr_, RAW(x), sizeof(T*));
+
+        } else if (TYPEOF(x) == REALSXP && Rf_length(x) == 1) {
+            // User passed a double(1) of the static-casted pointer address.
+            ptr_ = reinterpret_cast<T*>(static_cast<uintptr_t>(REAL(x)[0]));
+
+        } else {
+            Rcpp::stop("Can't convert input object to pointer");
+        }
+    }
+
+    inline operator SEXP() const { return R_MakeExternalPtr(ptr_, R_NilValue, R_NilValue); }
+
+    inline operator T*() const { return ptr_; }
+
+    inline void finalize() { delete ptr_; }
+
+    inline T* get() const { return ptr_; }
+
     T* ptr_;
 };
+
 
 #if TILEDB_VERSION >= TileDB_Version(2,2,0)
 // these functions are local to this compilation unit as is the defintion of Pointer
