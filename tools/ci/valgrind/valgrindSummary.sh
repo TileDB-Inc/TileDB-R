225236a247bff14060fd9e1467f74b4c92d59d5e
Dirk Eddelbuettel <edd@debian.org>
2022-03-25 08:22:13 -0500

    correct variable expansion with quotes, pass url explicitly

--- tools/ci/valgrind/valgrindSummary.sh
+++ tools/ci/valgrind/valgrindSummary.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ver="<unknown>"
-hook="${SLACK_WEBHOOK_SECRET}"
+ver=""
+hook=""
 
 if [ $# -ge 1 ]; then
-     ver=$1
-     #echo "Version is ${ver}."
+    ver=$1
+    hook=$2
+    #echo "Version is ${ver}."
 fi
 
 ## check for successful, or unsuccessful, output and use either
@@ -22,7 +23,7 @@ for suffix in Rout Rout.fail; do
         ## their sum is non-negative, emit a summary string
         lostsummary=$(cat ${file} | tr -d [,] | awk -f tools/ci/valgrind/sumLosses.awk )
 
-        if [ ${lostsummary} != "" ] && [ ${hook} != "" ]; then
+        if [ "${lostsummary}" != "" ] && [ "${hook}" != "" ]; then
             curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Running *$ver*:\n $lostsummary\"}" https://hooks.slack.com/services/"${hook}"
             echo ""
             echo "Message '${ver}' : '${lostsummary}' sent"
