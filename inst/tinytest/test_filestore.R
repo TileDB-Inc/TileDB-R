292adee5c3dc1d3ed3255e509e60f99d4f98bab1
Dirk Eddelbuettel <edd@debian.org>
2022-06-28 06:46:33 -0500

    Refine unit tests setup (#436)
    * skip tail of test_timetravel unless CI seen
    
    * skip filestore test if on old Windows server (R 4.1.* at CRAN)

--- inst/tinytest/test_filestore.R
+++ inst/tinytest/test_filestore.R
@@ -1,6 +1,9 @@
 library(tinytest)
 library(tiledb)
 
+isOldWindows <- Sys.info()[["sysname"]] == "Windows" && grepl('Windows Server 2008', osVersion)
+if (isOldWindows) exit_file("skip this file on old Windows releases")
+
 ctx <- tiledb_ctx(limitTileDBCores())
 
 if (tiledb_version(TRUE) < "2.9.0") exit_file("Needs TileDB 2.9.* or later")
