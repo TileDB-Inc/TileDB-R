75df7cd4b439a4c81efcf733511092caa20b8a42
Dirk Eddelbuettel <edd@debian.org>
2023-01-06 09:58:55 -0600

    Update scale filter option setter for factor, offset and bytewidth

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -192,8 +192,8 @@ libtiledb_filter_get_option <- function(filter, filter_option_str) {
     .Call(`_tiledb_libtiledb_filter_get_option`, filter, filter_option_str)
 }
 
-libtiledb_filter_set_option <- function(filter, filter_option_str, value) {
-    .Call(`_tiledb_libtiledb_filter_set_option`, filter, filter_option_str, value)
+libtiledb_filter_set_option <- function(filter, filter_option_str, valuesxp) {
+    .Call(`_tiledb_libtiledb_filter_set_option`, filter, filter_option_str, valuesxp)
 }
 
 libtiledb_filter_list <- function(ctx, filters) {
