0b87ba7692ae860df52f30aa044105ad3c90e3b9
Dirk Eddelbuettel <edd@debian.org>
2023-09-24 08:53:08 -0500

    Correct one-char typo [ci skip]

--- src/nullable.cpp
+++ src/nullable.cpp
@@ -1,6 +1,6 @@
 //  MIT License
 //
-//  Copyright (c) 2021-2023s TileDB Inc.
+//  Copyright (c) 2021-2023 TileDB Inc.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
