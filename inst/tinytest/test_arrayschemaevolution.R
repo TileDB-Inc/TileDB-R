04d8053f988b115681079c2f87efca05f722a6e1
Dirk Eddelbuettel <edd@debian.org>
2023-11-27 14:24:48 -0600

    Skip test_arrayschemaevolution.R on macOS

--- inst/tinytest/test_arrayschemaevolution.R
+++ inst/tinytest/test_arrayschemaevolution.R
@@ -3,6 +3,7 @@ library(tiledb)
 
 #isOldWindows <- Sys.info()[["sysname"]] == "Windows" && grepl('Windows Server 2008', osVersion)
 if (Sys.info()[["sysname"]] == "Windows") exit_file("Skip on Windows")
+if (Sys.info()[["sysname"]] == "Darwin") exit_file("Skip on Darwin")
 
 ctx <- tiledb_ctx(limitTileDBCores())
 
