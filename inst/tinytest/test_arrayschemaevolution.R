748704b3e04be142689e4118edee77c30b3207a2
Dirk Eddelbuettel <edd@debian.org>
2024-04-17 12:01:45 -0500

    Small refactoring of existing tests (#698)
    * Minor refactoring of tests into three fewer files, same tests
    
    * Remove additional ctx calls
    
    * Skip timetravel_extra
    
    * Skip tiledbarray_extra
    
    * Turn timetravel extra tests back on
    
    * Update NEWS and roll micro release [ci skip]

--- inst/tinytest/test_arrayschemaevolution.R
+++ inst/tinytest/test_arrayschemaevolution.R
@@ -210,3 +210,48 @@ for (tp in c("INT8", "UINT8")) {
         expect_silent(fromDataFrame(df, uri, mode="append", col_index=1)) 	# passes for UINT8
     }
 }
+
+## tests formerly in file 'test_arrayschemaevolution_arrow.R'
+if (Sys.info()[["sysname"]] == "Windows") exit_file("Skip on Windows")
+if (Sys.getenv("CI", "") == "") exit_file("Skip unextended test run")
+
+if (tiledb_version(TRUE) < "2.17.0") exit_file("Needs TileDB 2.17.* or later")
+
+df <- data.frame(key=letters[1:10], val=c(1:5,5:1))
+uri <- tempfile()
+arr <- fromDataFrame(df, uri)
+
+sch <- schema(uri)
+attrs <- attrs(sch)
+attr <- attrs$val   # copy of attribute
+
+## First drop existing attribute
+ase <- tiledb_array_schema_evolution()
+ase <- tiledb_array_schema_evolution_drop_attribute(ase, "val")
+tiledb_array_schema_evolution_array_evolve(ase, uri)
+
+## Second add enumeration under a name
+ase <- tiledb_array_schema_evolution()
+enums <- c("blue", "green", "orange", "pink", "red")
+ase <- tiledb_array_schema_evolution_add_enumeration(ase, "frobo", enums)
+
+## Third connect the attribute to the enum and add it back in
+attr <- tiledb_attribute_set_enumeration_name(attr, "frobo")
+ase <- tiledb_array_schema_evolution_add_attribute(ase, attr)
+tiledb_array_schema_evolution_array_evolve(ase, uri)
+
+## check as data.frame
+arr <- tiledb_array(uri, return_as="data.frame")
+res <- arr[]
+expect_true(is.factor(res$val))
+expect_equal(levels(res$val), enums)
+expect_equal(as.integer(res$val), c(1:5,5:1))
+
+## check as arrow
+if (!requireNamespace("arrow", quietly=TRUE)) exit_file("No 'arrow' package.")
+arr <- tiledb_array(uri, return_as="arrow")
+res <- arr[]
+v <- res[["val"]]$as_vector()
+expect_true(is.factor(v))
+expect_equal(levels(v), enums)
+expect_equal(as.integer(v), c(1:5,5:1))
--- inst/tinytest/test_timetravel.R
+++ inst/tinytest/test_timetravel.R
@@ -155,3 +155,43 @@ fi <- tiledb_fragment_info(uri)
 expect_equal(as.numeric(tiledb_fragment_info_get_timestamp_range(fi,0))*1000, c(1,1))
 expect_equal(as.numeric(tiledb_fragment_info_get_timestamp_range(fi,1))*1000, c(2,2))
 expect_equal(as.numeric(tiledb_fragment_info_get_timestamp_range(fi,2))*1000, c(3,3))
+
+
+## tests formerly in test_timetravel_extra.R
+
+## earlier time travel test recast via timestamp_{start,end}
+## time travel
+
+tmp <- tempfile()
+dir.create(tmp)
+dom <- tiledb_domain(dims = c(tiledb_dim("rows", c(1L, 10L), 5L, "INT32"),
+                              tiledb_dim("cols", c(1L, 10L), 5L, "INT32")))
+schema <- tiledb_array_schema(dom, attrs=c(tiledb_attr("a", type = "INT32")), sparse = TRUE)
+invisible( tiledb_array_create(tmp, schema) )
+
+I <- c(1, 2, 2)
+J <- c(1, 4, 3)
+data <- c(1L, 2L, 3L)
+now1 <- Sys.time()
+A <- tiledb_array(uri = tmp, timestamp_start=now1)
+A[I, J] <- data
+
+deltat <- 0.1
+epst <- deltat/2
+Sys.sleep(deltat)
+
+I <- c(8, 6, 9)
+J <- c(5, 7, 8)
+data <- c(11L, 22L, 33L)
+now2 <- Sys.time()
+A <- tiledb_array(uri = tmp, timestamp_start=now2)
+A[I, J] <- data
+
+A <- tiledb_array(uri = tmp, return_as="data.frame", timestamp_end=now1 - epst)
+expect_equal(nrow(A[]), 0)
+A <- tiledb_array(uri = tmp, return_as="data.frame", timestamp_start=now1 + epst)
+expect_equal(nrow(A[]), 3)
+A <- tiledb_array(uri = tmp, return_as="data.frame", timestamp_start=now1 - epst, timestamp_end=now2 - epst)
+expect_equal(nrow(A[]), 3)
+A <- tiledb_array(uri = tmp, return_as="data.frame", timestamp_start=now1 - epst, timestamp_end=now2 + epst)
+expect_true(nrow(A[]) >= 3)
