ee2b20e997d4c1d899966e2f99df1ee48b204a54
Paul Hoffman <mojaveazure@users.noreply.github.com>
2024-11-13 15:24:56 -0500

    Update windows install to include libtiledb source

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -5,14 +5,21 @@ if (!file.exists(dcffile)) stop("TileDB Version file not found.")
 dcf <- read.dcf(dcffile)
 ver <- dcf[[1, "version"]]
 
-if (!file.exists("../windows/rwinlib-tiledb/include/tiledb/tiledb.h")) {
+# if (!file.exists("../windows/rwinlib-tiledb/include/tiledb/tiledb.h")) {
+if (!file.exists("../inst/tiledb/include/tiledb/tiledb.h")) {
     if (getRversion() < "4") stop("This package requires Rtools40 or newer")
-    op <- options()
-    options(timeout=180)                # CRAN request to have patient download settings
-    download.file(sprintf("https://github.com/TileDB-Inc/rwinlib-tiledb/archive/v%s.zip", ver), "lib.zip", quiet = TRUE)
+    op <- options(timeout = 180) # CRAN request to have patient download settings
+    zipfile <- tempfile(tmpdir = tempdir(check = TRUE), fileext = '.zip')
+    download.file(
+      sprintf("https://github.com/TileDB-Inc/rwinlib-tiledb/archive/v%s.zip", ver),
+      destfile = zipfile,
+      quiet = TRUE
+    )
     options(op)
-    dir.create("../windows", showWarnings = FALSE)
-    unzip("lib.zip", exdir = "../windows")
-    file.rename(sprintf("../windows/rwinlib-tiledb-%s", ver), "../windows/rwinlib-tiledb")
-    unlink("lib.zip")
+    # dir.create("../windows", showWarnings = FALSE)
+    # unzip("lib.zip", exdir = "../windows")
+    unzip(zipfile, exdir = "../inst")
+    # file.rename(sprintf("../windows/rwinlib-tiledb-%s", ver), "../windows/rwinlib-tiledb")
+    file.rename(sprintf("../inst/rwinlib-tiledb-%s", ver), "../inst/tiledb")
+    unlink(zipfile, force = TRUE)
 }
