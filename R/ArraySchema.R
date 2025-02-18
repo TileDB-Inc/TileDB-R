4847b6b5fa38db839765865bab9e3e51c7b6ebb7
John Kerl <kerl.john.r@gmail.com>
2025-02-18 16:15:01 -0500

    Apply suggestions from code review
    Co-authored-by: Paul Hoffman <mojaveazure@users.noreply.github.com>

--- R/ArraySchema.R
+++ R/ArraySchema.R
@@ -594,7 +594,7 @@ tiledb_schema_get_names <- function(sch) {
   attrs <- tiledb::attrs(sch)
   attrnames <- unname(sapply(attrs, function(a) libtiledb_attribute_get_name(a@ptr)))
 
-  c(dimnames, attrnames)
+  return(c(dimnames, attrnames))
 }
 
 ##' Get all Dimension and Attribute Types
@@ -612,7 +612,7 @@ tiledb_schema_get_types <- function(sch) {
   attrs <- tiledb::attrs(sch)
   attrtypes <- unname(sapply(attrs, function(a) libtiledb_attribute_get_type(a@ptr)))
 
-  c(dimtypes, attrtypes)
+  return(c(dimtypes, attrtypes))
 }
 
 ##' Get Dimension or Attribute Status
