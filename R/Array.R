c74fcab03391780d60522098315dff9a12e71706
John Kerl <kerl.john.r@gmail.com>
2025-02-18 16:11:10 -0500

    Add `tiledb_array_is_open_for_reading()/writing()`

--- R/Array.R
+++ R/Array.R
@@ -136,6 +136,28 @@ tiledb_array_is_open <- function(arr) {
   libtiledb_array_is_open(arr@ptr)
 }
 
+##' Test if TileDB Array is open for reading
+##'
+##' @param arr A TileDB Array object as for example returned by `tiledb_array()`
+##' @return A boolean indicating whether the TileDB Array object is open, and
+##' the mode is "READ".
+##' @export
+tiledb_array_is_open_for_reading <- function(arr) {
+  stopifnot("The 'arr' argument must be a tiledb_array object" = .isArray(arr))
+  libtiledb_array_is_open(arr@ptr) && tiledb:::libtiledb_array_query_type(arr@ptr) == "READ"
+}
+
+##' Test if TileDB Array is open for writing
+##'
+##' @param arr A TileDB Array object as for example returned by `tiledb_array()`
+##' @return A boolean indicating whether the TileDB Array object is open, and
+##' the mode is "WRITE".
+##' @export
+tiledb_array_is_open_for_writing <- function(arr) {
+  stopifnot("The 'arr' argument must be a tiledb_array object" = .isArray(arr))
+  libtiledb_array_is_open(arr@ptr) && tiledb:::libtiledb_array_query_type(arr@ptr) == "WRITE"
+}
+
 ##' Check for Homogeneous Domain
 ##'
 ##' @param arr A TileDB Array object
