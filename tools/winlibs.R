9621b419ac83248cbceb246ffd1f65bb5534a420
Paul Hoffman <mojaveazure@users.noreply.github.com>
2025-04-24 12:55:58 -0400

    Remove static libraries from final build of tiledb-r on Windows (#816)
    * Remove static libraries from final build of tiledb-r on Windows
    R 4.5 has begun introducing checks of static libraries bundled as part
    of the package; this PR removes said static libraries as they were
    failing CRAN checks
    
    * Update changelog
    Bump develop version

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -5,17 +5,18 @@ if (!file.exists(dcffile)) stop("TileDB Version file not found.")
 dcf <- read.dcf(dcffile)
 ver <- dcf[[1, "version"]]
 
-if (!file.exists("../inst/tiledb/include/tiledb/tiledb.h")) {
-    if (getRversion() < "4") stop("This package requires Rtools40 or newer")
-    op <- options(timeout = 180) # CRAN request to have patient download settings
-    zipfile <- tempfile(tmpdir = tempdir(check = TRUE), fileext = '.zip')
-    download.file(
-      sprintf("https://github.com/TileDB-Inc/rwinlib-tiledb/archive/v%s.zip", ver),
-      destfile = zipfile,
-      quiet = !isTRUE(getOption('verbose', default = FALSE))
-    )
-    options(op)
-    unzip(zipfile, exdir = "../inst")
-    file.rename(sprintf("../inst/rwinlib-tiledb-%s", ver), "../inst/tiledb")
-    unlink(zipfile, force = TRUE)
+if (!file.exists("../inst/tiledb/include/tiledb/tiledb.h") || !dir.exists("../inst/tiledb/lib/")) {
+  if (getRversion() < "4") stop("This package requires Rtools40 or newer")
+  if (dir.exists("../inst/tiledb")) unlink("../inst/tiledb", recursive = TRUE, force = TRUE)
+  op <- options(timeout = 180) # CRAN request to have patient download settings
+  zipfile <- tempfile(tmpdir = tempdir(check = TRUE), fileext = '.zip')
+  download.file(
+    sprintf("https://github.com/TileDB-Inc/rwinlib-tiledb/archive/v%s.zip", ver),
+    destfile = zipfile,
+    quiet = !isTRUE(getOption('verbose', default = FALSE))
+  )
+  options(op)
+  unzip(zipfile, exdir = "../inst")
+  file.rename(sprintf("../inst/rwinlib-tiledb-%s", ver), "../inst/tiledb")
+  unlink(zipfile, force = TRUE)
 }
